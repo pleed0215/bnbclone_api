276f24ac52ff244cf67a0c8ca26b85e4b45e2282
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-09-09 21:29:51 +0900

    haha

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = "users"
 
-urlpatterns = []
+urlpatterns = [
+    path("me/", views, name="me"),
+    path("<int:pk>", views, name="profile"),
+]
